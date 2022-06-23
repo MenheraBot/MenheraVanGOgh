71b87fda8690cb22946585928bdf504d52fb6c5b
Luxanna <CommandPlayermine@gmail.com>
2022-06-23 08:42:26 -0300

    chore: remove comments

--- src/renderers/profiles/kawaii.go
+++ src/renderers/profiles/kawaii.go
@@ -52,31 +52,6 @@ func RenderKawaii(User *utils.UserData, I18n *utils.I18n, util utils.Utils) imag
 	ctx.DrawStringWrapped(I18n.Usages+"   | "+strconv.Itoa(User.Votes)+" Upvotes", 85, 580, 0, 0.5, 650, 1, 0)
 
 	util.DrawBadges(ctx, User, 410, 40)
-	/*
-		util.DrawBadges(ctx, User, 408, 435)
 
-		ctx.DrawStringWrapped(User.Tag, 425, 160, 0, 1, 420, 0, 0)
-
-		ctx.SetFontFace(*util.GetFont("Pixellari", 24))
-		ctx.DrawStringWrapped(User.Info, 200, 540, 0, 0.5, 700, 1, 0)
-
-		if User.Married {
-			ctx.SetFontFace(*util.GetFont("Pixellari", 20))
-			ctx.DrawStringWrapped(User.Marry.Username+" "+strings.Split(User.MarryDate, " ")[0], 445, 220, 0, 1, 600, 1, 0)
-		}
-
-		fontSize := 24
-
-		if User.Mamou >= 1000 || User.Mamadas >= 100 {
-			fontSize = 22
-		}
-
-		ctx.SetFontFace(*util.GetFont("Pixellari", float64(fontSize)))
-		ctx.DrawStringAnchored(I18n.Mamado+": "+strconv.Itoa(User.Mamadas), 850, 145, 0, 0)
-		ctx.DrawStringAnchored(I18n.Mamou+": "+strconv.Itoa(User.Mamou), 850, 175, 0, 0)
-
-		ctx.SetFontFace(*util.GetFont("Pixellari", 24))
-		ctx.DrawStringWrapped(strings.Split(I18n.Usages, ".")[0]+" | "+strconv.Itoa(User.Votes)+" Upvotes", 390, 426, 0, 1, 540, 1, 0)
-	*/
 	return ctx.Image()
 }
