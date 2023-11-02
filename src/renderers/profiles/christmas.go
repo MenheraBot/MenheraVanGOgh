588d223fdb8dba5d3c80fb09d7a0f673ffa85d7a
ySnoopyDogy <CommandPlayermine@gmail.com>
2023-11-02 13:49:33 -0300

    feat: implement user title in christmas

--- src/renderers/profiles/christmas.go
+++ src/renderers/profiles/christmas.go
@@ -13,8 +13,6 @@ import (
 func RenderChristmas(User *utils.UserData, I18n *utils.I18n, customEdits []string, db *database.Database) image.Image {
 	ctx := gg.NewContext(1080, 720)
 
-	//baseColor :=
-
 	if utils.GetProfileCustomization("useImage", customEdits) {
 		backgroundImage := utils.GetImageFromURL(User.Image, 1080, 720, db)
 
@@ -35,40 +33,32 @@ func RenderChristmas(User *utils.UserData, I18n *utils.I18n, customEdits []strin
 		ctx.Stroke()
 	}
 
-	userAvatar := utils.GetImageFromURL(User.Avatar, 250, 250, db)
-
-	ctx.DrawCircle(193, 133, 125)
-	ctx.Clip()
-	ctx.DrawImageAnchored(userAvatar, 193, 133, 0.5, 0.5)
-	ctx.ResetClip()
-
 	backgroundImage := utils.GetAsset("/profiles/christmas.png")
 
 	ctx.DrawImage(backgroundImage, 0, 0)
 
+	userAvatar := utils.GetImageFromURL(User.Avatar, 255, 255, db)
+	ctx.DrawCircle(195, 133, 127.5)
+	ctx.Clip()
+	ctx.DrawImageAnchored(userAvatar, 195, 133, 0.5, 0.5)
+	ctx.ResetClip()
+
+	ctx.SetFontFace(*utils.GetFont("Candy", float64(28)))
+	utils.StrokeText(ctx, User.Username, 660, 130, 2, 0.5, 0, "#FFF")
 	ctx.SetHexColor("#FF0000")
+	ctx.DrawStringAnchored(User.Username, 660, 130, 0.5, 0)
 
-	fontSize := 40
+	ctx.SetHexColor("#fff")
+	ctx.SetFontFace(*utils.GetFont("Sans", 24))
+	ctx.DrawStringAnchored(User.Title, 660, 80, 0.5, 0)
 
-	if len(User.Username) > -18 {
-		fontSize = 28
+	if User.Married {
+		ctx.DrawStringWrapped(User.MarryUsername+" "+strings.Split(User.MarryDate, " ")[0], 400, 160, 0, 1, 600, 1, 0)
 	}
-
-	ctx.SetFontFace(*utils.GetFont("Candy", float64(fontSize)))
-
-	utils.StrokeText(ctx, User.Username, 660, 100, 2, 0.5, 0, "#FFF")
-
-	ctx.SetHexColor("#FF0000")
-	ctx.DrawStringAnchored(User.Username, 660, 100, 0.5, 0)
 
 	ctx.SetHexColor(utils.GetCompatibleFontColor(User.Color))
 	ctx.SetFontFace(*utils.GetFont("Impact", 32))
 	ctx.DrawStringWrapped(User.Info, 90, 540, 0, 1, 920, 1, 0)
-
-	if User.Married {
-		ctx.SetFontFace(*utils.GetFont("Sans", 24))
-		ctx.DrawStringWrapped(User.MarryUsername+" "+strings.Split(User.MarryDate, " ")[0], 400, 140, 0, 1, 600, 1, 0)
-	}
 
 	ctx.SetFontFace(*utils.GetFont("Sans", 32))
 	ctx.DrawStringAnchored(I18n.Mamado+": "+strconv.Itoa(int(User.Mamadas))+" "+I18n.Mamou+": "+strconv.Itoa(int(User.Mamou)), 380, 243, 0, 0)
@@ -77,7 +67,7 @@ func RenderChristmas(User *utils.UserData, I18n *utils.I18n, customEdits []strin
 
 	ctx.DrawStringWrapped(I18n.Usages+"   | "+strconv.Itoa(int(User.Votes))+" Upvotes", 90, 270, 0, 0, 920, 1, 0)
 
-	utils.DrawBadges(ctx, User, 80, 667)
+	utils.DrawBadges(ctx, User, 80, 656)
 
 	return ctx.Image()
 }
